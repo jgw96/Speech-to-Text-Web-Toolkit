5baa97a098d0d197923339d6f4cce69d5a7b76c9
jgw96 <jgw9617@gmail.com>
2024-03-04 15:08:39 -0800

    basic

--- src/my-element.ts
+++ src/my-element.ts
@@ -1,21 +1,13 @@
-/**
- * @license
- * Copyright 2019 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { LitElement, html, css } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
 
-import {LitElement, html, css} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+/*
+  TODO: Fix importing of speech-sdk, which would also fix types
+*/
 
-/**
- * An example element.
- *
- * @fires count-changed - Indicates when the count changes
- * @slot - This element has a slot
- * @csspart button - The button
- */
-@customElement('my-element')
-export class MyElement extends LitElement {
+@customElement('speech-to-text')
+export class SpeechToText extends LitElement {
   static override styles = css`
     :host {
       display: block;
@@ -25,44 +17,142 @@ export class MyElement extends LitElement {
     }
   `;
 
-  /**
-   * The name to say "Hello" to.
-   */
-  @property()
-  name = 'World';
+  @property({ type: String }) apiKey = 'a3484733425e4929ae1da1f90a5f0a16';
+  @property({ type: String }) region = 'eastus';
+  @property({ type: String }) language = 'en-US';
 
-  /**
-   * The number of times the button has been clicked.
-   */
-  @property({type: Number})
-  count = 0;
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  @state() private sdk: any = window.SpeechSDK;
+
+  audioConfig: any | undefined = undefined;
+  speechConfig: any | undefined = undefined;
+  recog: any | undefined = undefined;
+
+  lines: string[] | undefined = undefined;
+  finalizedLines: string[] | undefined = undefined;
+  keyPoints: any[] | undefined = undefined;
+  transcript: string | undefined = undefined;
+  focusedLine: string | undefined = undefined;
 
   override render() {
     return html`
-      <h1>${this.sayHello(this.name)}!</h1>
-      <button @click=${this._onClick} part="button">
-        Click Count: ${this.count}
-      </button>
       <slot></slot>
     `;
   }
 
-  private _onClick() {
-    this.count++;
-    this.dispatchEvent(new CustomEvent('count-changed'));
+  override async firstUpdated() {
+    if (this.sdk) {
+      this.audioConfig = this.sdk.AudioConfig.fromDefaultMicrophoneInput();
+      this.speechConfig = this.sdk.SpeechConfig.fromSubscription(this.apiKey, this.region);
+
+      this.speechConfig!.speechRecognitionLanguage = this.language;
+      this.speechConfig!.enableDictation();
+
+      this.recog = new this.sdk.SpeechRecognizer(this.speechConfig, this.audioConfig);
+
+      await this.setUpListeners();
+    }
   }
 
-  /**
-   * Formats a greeting
-   * @param name The name to say "Hello" to
-   */
-  sayHello(name: string): string {
-    return `Hello, ${name}`;
+  private async setUpListeners() {
+    this.lines = [];
+    this.finalizedLines = [];
+    this.keyPoints = [];
+
+    if (this.recog) {
+      this.recog.recognizing = (s?: any, e?: any) => {
+        console.log("s", s);
+        console.log("s.results", e!.result);
+
+        this.transcript = e!.result.text;
+        if (this.lines && this.finalizedLines) {
+          if (this.lines.length && this.lines[this.lines.length - 1] !== this.finalizedLines[this.finalizedLines.length - 1]) {
+            // remove the last line from this.lines
+            this.lines = this.lines.slice(0, this.lines.length - 1);
+          }
+
+          this.lines = [...this.lines, e!.result.text];
+
+          this.requestUpdate();
+
+          this.dispatchEvent(new CustomEvent('recognized-interim', {
+            detail: {
+              message: this.lines.join(' ')
+            }
+          }));
+        }
+
+      };
+
+      this.recog.recognized = async (s?: any, e?: any) => {
+        console.log(s);
+        console.log('recognized', e!.result.text);
+
+        if (e!.result.text && e!.result.text.length > 0) {
+          this.finalizedLines = [...this.finalizedLines!, e!.result.text];
+          this.lines = [...this.finalizedLines];
+
+          this.focusedLine = e!.result.text;
+
+          this.requestUpdate();
+
+          this.dispatchEvent(new CustomEvent('recognized', {
+            detail: {
+              message: this.lines.join(' ')
+            }
+          }));
+        }
+      }
+
+      this.recog.canceled = async (s?: any, e?: any) => {
+        console.log(`CANCELED: Reason=${e!.reason}, ${s}`);
+
+        this.dispatchEvent(new CustomEvent('transcribe-canceled', {
+          detail: {
+            message: `CANCELED: ErrorCode=${e!.errorCode}`
+          }
+        }));
+
+        await this.recog!.stopContinuousRecognitionAsync();
+      };
+
+      this.recog.sessionStopped = async (s?: any, e?: any) => {
+        console.log("\n    Session stopped event.", s, e);
+        await this.recog!.stopContinuousRecognitionAsync();
+
+        this.dispatchEvent(new CustomEvent('transcribe-stopped', {
+          detail: {
+            message: 'Speech to text stopped'
+          }
+        }));
+      };
+    }
+  }
+
+  public async startSpeechToText() {
+    this.recog!.startContinuousRecognitionAsync();
+
+    this.dispatchEvent(new CustomEvent('transcribe-started', {
+      detail: {
+        message: 'Speech to text started'
+      }
+    }));
+  }
+
+  public async stopSpeechToText() {
+    this.recog!.stopContinuousRecognitionAsync();
+
+    this.dispatchEvent(new CustomEvent('transcribe-stopped', {
+      detail: {
+        message: 'Speech to text stopped'
+      }
+    }));
   }
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'my-element': MyElement;
+    'speech-to-text': SpeechToText;
   }
 }
